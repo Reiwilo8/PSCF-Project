724e11cb4cefe8079e25074fb854f3b8e5b7c849
Reiwilo8 <kddfat@jadamspam.pl>
2025-06-12 20:32:24 +0200

    Implemented additive AI scene in settings

--- Assets/Scripts/UI/SettingsSceneUI.cs
+++ Assets/Scripts/UI/SettingsSceneUI.cs
@@ -9,10 +9,12 @@ public class SettingsSceneUI : MonoBehaviour
     private void Start()
     {
         backButton.onClick.AddListener(OnBackClicked);
+        SceneManager.LoadScene("SettingsAIScene", LoadSceneMode.Additive);
     }
 
     private void OnBackClicked()
     {
+        SceneManager.UnloadSceneAsync("SettingsAIScene");
         SceneManager.LoadScene("StartScreen");
     }
 }
