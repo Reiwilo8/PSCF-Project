b311336991d55302edaac1966ac7be693ac7f4dd
Reiwilo8 <kddfat@jadamspam.pl>
2025-06-13 13:54:55 +0200

    Implemented transitions in Settings

--- Assets/Scripts/UI/SettingsSceneUI.cs
+++ Assets/Scripts/UI/SettingsSceneUI.cs
@@ -1,23 +1,69 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SettingsSceneUI : MonoBehaviour
 {
     public Button backButton;
+    public Button profileButton;
+    public Button aiButton;
+
+    private string currentScene;
+    private bool isTransitioning = false;
 
     private void Start()
     {
+        currentScene = "SettingsProfileScene";
+
         backButton.onClick.AddListener(OnBackClicked);
-        SceneManager.LoadScene("SettingsProfileScene", LoadSceneMode.Additive);
-        //SceneManager.LoadScene("SettingsAIScene", LoadSceneMode.Additive);
+        profileButton.onClick.AddListener(() => SwitchToScene("SettingsProfileScene"));
+        aiButton.onClick.AddListener(() => SwitchToScene("SettingsAIScene"));
+
+        StartCoroutine(LoadInitialScene());
+    }
+
+    private IEnumerator LoadInitialScene()
+    {
+        var asyncLoad = SceneManager.LoadSceneAsync(currentScene, LoadSceneMode.Additive);
+        yield return asyncLoad;
+
+        var loadedScene = SceneManager.GetSceneByName(currentScene);
+        if (loadedScene.IsValid())
+            SceneManager.SetActiveScene(loadedScene);
+
+        UpdateButtonStates();
     }
 
     private void OnBackClicked()
     {
-        UnloadIfLoaded("SettingsProfileScene");
-        UnloadIfLoaded("SettingsAIScene");
+        UnloadIfLoaded(currentScene);
         SceneManager.LoadScene("StartScreen");
+    }
+
+    private async void SwitchToScene(string targetScene)
+    {
+        if (isTransitioning || currentScene == targetScene)
+            return;
+
+        isTransitioning = true;
+
+        SetAllButtonsInteractable(false);
+
+        if (SceneManager.GetSceneByName(currentScene).isLoaded)
+        {
+            await SceneManager.UnloadSceneAsync(currentScene);
+        }
+
+        currentScene = targetScene;
+        await SceneManager.LoadSceneAsync(currentScene, LoadSceneMode.Additive);
+
+        var loadedScene = SceneManager.GetSceneByName(currentScene);
+        if (loadedScene.IsValid())
+            SceneManager.SetActiveScene(loadedScene);
+
+        UpdateButtonStates();
+        isTransitioning = false;
     }
 
     private void UnloadIfLoaded(string sceneName)
@@ -27,5 +73,19 @@ public class SettingsSceneUI : MonoBehaviour
         {
             SceneManager.UnloadSceneAsync(scene);
         }
+    }
+
+    private void UpdateButtonStates()
+    {
+        profileButton.interactable = currentScene != "SettingsProfileScene";
+        aiButton.interactable = currentScene != "SettingsAIScene";
+        backButton.interactable = true;
+    }
+
+    private void SetAllButtonsInteractable(bool state)
+    {
+        profileButton.interactable = state;
+        aiButton.interactable = state;
+        backButton.interactable = state;
     }
 }
